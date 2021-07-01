59c72b58ac6fae92d041498494ce4a9e4809cc56
AvezzaniNicolas <nicolas.avezzanirc@gmail.com>
2021-07-01 16:08:57 -0300

    hola

---
+++ index.js
@@ -0,0 +1,81 @@
+addEventListener("load", load);
+//llamo al servidor.
+//var servi = "http://localhost:444/login";
+var servi = "https://serviback.herokuapp.com/login";
+
+ //var servi = "https://serviback.herokuapp.com/frontend";
+
+function $(demo){
+    return document.getElementById(demo);
+}
+
+
+
+function load(){
+    document.getElementById("ingresar").addEventListener("click", click);
+}
+
+function click(){
+   enviarMensajeAlServidorPost(servi,retornoDelClick);
+
+}
+
+function retornoDelClick(respuesta){
+    if(respuesta == 'Bienvenido'){
+        location.replace("pantallaprincipal.html");
+    }else{
+        alert('error al ingresar');
+    }
+}
+
+function enviarMensajeAlServidor(servi, funcionARealizar){
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET",servi ,true);
+
+    xmlhttp.onreadystatechange = function(){
+
+        if(xmlhttp.readyState == XMLHttpRequest.DONE){
+            if(xmlhttp.status == 200){
+                console.log(xmlhttp.response);
+                funcionARealizar(xmlhttp.responseText);
+            }else{
+                alert("Ocurrio un error");
+            }
+        }
+
+    }
+    xmlhttp.send();    
+}
+
+
+function enviarMensajeAlServidorPost(servidor, funcionARealizar) {
+
+    //declaro el objeto
+    var xmlhttp = new XMLHttpRequest();
+    var datos = new FormData();
+    datos.append("nombre",$("usuario").value);
+    datos.append("contrasena",$("contrasenia").value);
+
+    // indico hacia donde va el mensaje
+    xmlhttp.open("POST", servidor, true);
+    //seteo el evento
+    xmlhttp.onreadystatechange = function () {
+        //Veo si llego la respuesta del servidor
+        if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+            //Reviso si la respuesta es correcta
+            if (xmlhttp.status == 200) {
+                funcionARealizar(xmlhttp.responseText);
+            }
+            else {
+                alert("ocurrio un error");
+            }
+        }
+    }
+
+   
+    xmlhttp.setRequestHeader("enctype", "multipart/form-data");
+
+    //envio el mensaje    
+    xmlhttp.send(datos);
+}
